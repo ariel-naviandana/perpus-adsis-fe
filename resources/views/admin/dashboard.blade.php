bbbcb4f157634e31cdf307e59c6c52c8bc71dff0
Ariel2711 <arielnaviandanaputra@gmail.com>
2025-06-02 11:21:41 +0700

    update dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,7 +5,7 @@
     <h2>Dashboard Admin</h2>
 
     <div class="row mt-4">
-        <div class="col-md-4">
+        <div class="col-md-6">
             <div class="card text-white bg-primary mb-3">
                 <div class="card-header">Total Buku</div>
                 <div class="card-body">
@@ -14,20 +14,11 @@
             </div>
         </div>
 
-        <div class="col-md-4">
+        <div class="col-md-6">
             <div class="card text-white bg-success mb-3">
                 <div class="card-header">Total User</div>
                 <div class="card-body">
                     <h5 class="card-title">{{ $stats['total_users'] ?? 0 }}</h5>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-4">
-            <div class="card text-white bg-warning mb-3">
-                <div class="card-header">Total Petugas</div>
-                <div class="card-body">
-                    <h5 class="card-title">{{ $stats['total_petugas'] ?? 0 }}</h5>
                 </div>
             </div>
         </div>
